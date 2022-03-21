723e0feb418950a9f6a9c43f83e96905847a19ec
Kevin Cuadros <kevindavidcuadros@gmail.com>
2022-03-21 17:01:04 -0500

    descripcion

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { CalendarApp } from './CalendarApp';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-
 import './styles.css';
 
 ReactDOM.render(
     <CalendarApp />,
-  document.getElementById('root')
+    document.getElementById('root')
 );
-
-serviceWorkerRegistration.register();
